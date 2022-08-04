fefb5a62a60bb7af549d8bc25bdde12ea883871a
kfir4444 <kfir.k4@gmail.com>
2022-08-04 10:57:09 +0300

    removed double {snake_name} init bash to fix double conda issue

--- install_conda.sh
+++ install_conda.sh
@@ -14,5 +14,4 @@ wget -P /home/$USER/Downloads https://repo.anaconda.com/archive/Anaconda3-2021.1
 # Installation
 bash /home/$USER/Downloads/Anaconda3-2021.11-Linux-x86_64.sh -u
 export PATH=$PATH:$HOME/anaconda3/bin
-conda init bash
 source ~/.bashrc
