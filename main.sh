e601d1af61519b30c59fe3403eef5e73fa88276c
kfir4444 <kfir.k4@gmail.com>
2022-08-04 15:20:56 +0300

    removed install_mamba.sh, renamed to main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,20 @@
-# This script should be used when creating a new account for a user on our machines
-# after running install_2.sh and manually activating rmg_env
+# after running installing conda
+
+source ~/.bashrc
+
+# Install mamba (https://github.com/mamba-org/mamba, https://mamba.readthedocs.io/en/latest/user_guide/mamba.html)
+conda install mamba -n base -c conda-forge -y
+
+source ~/.bashrc
+
+# Clone repos
+cd /home/$USER/Code
+git clone https://github.com/ReactionMechanismGenerator/RMG-Py.git
+git clone https://github.com/ReactionMechanismGenerator/RMG-database.git
+git clone https://github.com/ReactionMechanismGenerator/ARC.git
+git clone https://github.com/ReactionMechanismGenerator/T3.git
+
+# Change remote names
 
 cd RMG-database
 git remote rename origin official
@@ -13,6 +28,8 @@ cd ../RMG-Py
 git remote rename origin official
 mamba env create -f environment.yml
 
+# Echo paths to ~/.bashrc
+
 echo 'export PYTHONPATH=$HOME/Code/RMG-Py/:$PYTHONPATH' >> ~/.bashrc
 echo 'export PATH=$HOME/Code/RMG-Py/:$PATH ' >> ~/.bashrc
 
@@ -24,12 +41,16 @@ echo 'export PATH=$HOME/Code/T3/:$PATH' >> ~/.bashrc
 
 source ~/.bashrc
 
+# Compile RMG
+
 conda activate rmg_env
 cd RMG-Py/
 make
 python -c "import julia; julia.install(); import diffeqpy; diffeqpy.install()"
 julia -e 'using Pkg; Pkg.add(PackageSpec(name="ReactionMechanismSimulator",version="0.4")); using ReactionMechanismSimulator;'
 conda deactivate
+
+# Install ARC's requirements.
 
 conda activate rmg_env
 cd ARC/
