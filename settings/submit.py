6a57570a6ff61dd66de27011ff8fa2b74f420402
kfir4444 <75449651+kfir4444@users.noreply.github.com>
2023-05-15 12:17:36 +0300

    Update submit.py

--- settings/submit.py
+++ settings/submit.py
@@ -4,21 +4,16 @@ Submit scripts and incore commands
 
 # commands to execute ESS incore (without cluster software submission)
 incore_commands = {
-    'xtb': ['CONDA_BASE=$(conda info --base)',
-            'source $CONDA_BASE/etc/profile.d/conda.sh',
-            'conda activate xtb_env',
+    'gaussian': ['g16 < input.gjf > input.log',
+                 'formchk check.chk check.fchk',
+                 ],
+    'xtb': [
             'bash input.sh',
             ],
-    'xtb_gsm': ['CONDA_BASE=$(conda info --base)',
-                'source $CONDA_BASE/etc/profile.d/conda.sh',
-                'conda activate xtb_env',
+    'xtb_gsm': ['micromamba activate xtb_env',
                 './gsm.orca',
                 ],
-    'sella': ['CONDA_BASE=$(conda info --base)',
-              'source $CONDA_BASE/etc/profile.d/conda.sh',
-              'conda activate sella_env',
-              'python sella_runner.py',
-              ],
+    'sella': ['/opt/conda/envs/sella_env/bin/python sella_runner.py'],
 }
 
 pipe_submit = {}
